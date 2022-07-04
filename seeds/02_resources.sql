6a8b1f77cdde0470c9c68392f9d47f6a9dfeed8b
megan <meganambermcbride@gmail.com>
2022-07-04 10:59:57 -0400

    Add resources.sql

---
+++ seeds/02_resources.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_id, title, url, description, image_url, date_created)
+VALUES (1, 'Creative ways to organize turn-in bin', 'https://www.weareteachers.com/classroom-turn-in-bin/?epik=dj0yJnU9cDhJYWpDSnJGTWdVZGVSZTczVnBkWHlDWGZVY2dqNG8mcD0wJm49T2IxVmN2TmQweWx4RzhSSmFfMXhIUSZ0PUFBQUFBR0xDX01J', 'description' ,'https://i.pinimg.com/564x/f4/40/3c/f4403c841ae3a4e89171a6cc0fcb96fc.jpg', '2022-01-20');
+
+INSERT INTO users (user_id, title, url, description, image_url, date_created)
+VALUES (2, 'Classroom Prep Checklist', 'https://resilienteducator.com/classroom-resources/ultimate-classroom-checklist/', 'description' ,'https://marvel-b1-cdn.bc0a.com/f00000000026007/resilienteducator.com/wp-content/uploads/2018/08/back-to-school-supplies-small.jpg', '2022-01-23');
