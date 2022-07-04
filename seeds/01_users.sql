741f9f042acf287c3dfa0f70b5d482b21e12cd85
megan <meganambermcbride@gmail.com>
2022-07-04 11:40:23 -0400

    lol lmao comment

--- seeds/01_users.sql
+++ seeds/01_users.sql
@@ -3,3 +3,5 @@ VALUES ('Lisa Frazer', 'lisaf2@gmail.com', 'lizzy_fraz', '$2a$10$FB/BOAVhpuLvpOR
 
 INSERT INTO users (name, email, username, password, profile_image, bio)
 VALUES ('Ryan Smith', 'rsmith@gmail.com', 'ryansmith', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ,'https://www.natureplprints.com/p/729/domestic-cat-kittens-age-3-weeks-garden-france-15336729.jpg', 'bio');
+
+-- LOL LMAO
