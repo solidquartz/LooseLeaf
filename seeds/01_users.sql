243d67f8d36e0c4d597f7ae5e01bde977b4fff41
megan <meganambermcbride@gmail.com>
2022-07-04 10:59:38 -0400

    Add user seeds

---
+++ seeds/01_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, username, password, profile_image, bio)
+VALUES ('Lisa Frazer', 'lisaf2@gmail.com', 'lizzy_fraz', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ,'https://www.natureplprints.com/p/729/domestic-cat-kittens-age-3-weeks-garden-france-15336729.jpg', 'bio');
+
+INSERT INTO users (name, email, username, password, profile_image, bio)
+VALUES ('Ryan Smith', 'rsmith@gmail.com', 'ryansmith', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ,'https://www.natureplprints.com/p/729/domestic-cat-kittens-age-3-weeks-garden-france-15336729.jpg', 'bio');
