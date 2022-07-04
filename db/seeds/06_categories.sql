fde54f5736dd4f85198ace866a8bee1defe7a97a
megan <meganambermcbride@gmail.com>
2022-07-04 15:25:14 -0400

    Rename tags to category and reformat

---
+++ db/seeds/06_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (name
+VALUES ('other');
+
+INSERT INTO categories (name)
+VALUES ('learning');
+
+INSERT INTO categories (name)
+VALUES ('games');
--- db/seeds/06_tags.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tags (user_id, name, resource_id)
-VALUES (1, other, 1);
-
-INSERT INTO tags (user_id, name, resource_id)
-VALUES (2, learning, 3);
