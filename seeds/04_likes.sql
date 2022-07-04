65eb86e9ef2d4a6c4b0917eeba05237681dcb80f
megan <meganambermcbride@gmail.com>
2022-07-04 11:20:37 -0400

    Add likes seeds

--- seeds/04_likes.sql
+++ seeds/04_likes.sql
@@ -1,9 +1,3 @@
-CREATE TABLE likes (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
-);
-
 INSERT INTO ratings (user_id, resource_id)
 VALUES (1, 3);
 
