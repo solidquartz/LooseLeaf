03001822b41f9164ce51a069751446f2aa761da8
megan <meganambermcbride@gmail.com>
2022-07-04 11:20:55 -0400

    Add tags seeds

--- seeds/06_tags.sql
+++ seeds/06_tags.sql
@@ -1,9 +1,3 @@
-CREATE TABLE tags (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
-);
-
 INSERT INTO tags (user_id, name, resource_id)
 VALUES (1, other, 1);
 
