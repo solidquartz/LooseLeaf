64468651cd9f997f59c2d70f8baddb3b5dd32399
megan <meganambermcbride@gmail.com>
2022-07-04 15:37:14 -0400

    Add category list seeds

--- db/seeds/06_categories.sql
+++ db/seeds/06_categories.sql
@@ -1,8 +1,56 @@
 INSERT INTO categories (name
-VALUES ('other');
+VALUES ('Other');
 
 INSERT INTO categories (name)
-VALUES ('learning');
+VALUES ('Learning');
 
 INSERT INTO categories (name)
-VALUES ('games');
+VALUES ('Administrative');
+
+INSERT INTO categories (name)
+VALUES ('Personal');
+
+INSERT INTO categories (name)
+VALUES ('Mental Health');
+
+INSERT INTO categories (name)
+VALUES ('Math');
+
+INSERT INTO categories (name)
+VALUES ('Education Resource');
+
+INSERT INTO categories (name)
+VALUES ('Language');
+
+INSERT INTO categories (name)
+VALUES ('Science');
+
+INSERT INTO categories (name)
+VALUES ('Games');
+
+INSERT INTO categories (name)
+VALUES ('Arts & Crafts');
+
+INSERT INTO categories (name)
+VALUES ('Engineering');
+
+INSERT INTO categories (name)
+VALUES ('Technology');
+
+INSERT INTO categories (name)
+VALUES ('Computer Science');
+
+INSERT INTO categories (name)
+VALUES ('Medicine');
+
+INSERT INTO categories (name)
+VALUES ('Culinary & Nutrition');
+
+INSERT INTO categories (name)
+VALUES ('Political Science');
+
+INSERT INTO categories (name)
+VALUES ('History');
+
+INSERT INTO categories (name)
+VALUES ('Literature');
