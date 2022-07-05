9c5bdaaa2cde640b16743d698402ad02538ba55f
megan <meganambermcbride@gmail.com>
2022-07-05 16:50:42 -0400

    Add login post

--- routes/login_register.js
+++ routes/login_register.js
@@ -1,74 +1,43 @@
+const { offset } = require('@popperjs/core');
 const express = require('express');
 const { user } = require('pg/lib/defaults');
 const router = express.Router();
 const helperFunctions = require('./helper_functions')
 
 module.exports = (db) => {
+
   const getUserByEmail = (email) => {
-    return db.query(`SELECT * FROM users`)
-    .then(res => {
-      let currentUser = null;
-      for(const user of res.rows) {
-        if(user.email.toLowerCase() === email.toLowerCase)
-        currentUser = user
-        break;
-      }
-      return user
-    })
-    .catch(err => {
-      return err.message
+    return db.query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then(user => {
+     return user
     })
   }
 
+  // login/register page get
   router.get("/", (req, res) => {
     const id = req.session.userID;
     res.render("login_register")
   })
 
-
+  // login post
   router.post("/login", (req, res) => {
     const { email, password } = req.body
-    console.log(req.body, "here hey are")
+    getUserByEmail(email)
+    .then(user => {
+      let userData = user.rows[0]
+      if(userData.password === password) {
+        req.session.userId = userData.id
+        return res.redirect("/profiles/:id")
+      }
+        res.send({error: "Invalid email or password"})
+        return;
+    })
   })
-  router.post("/register", (req, res) => {})
+
+  // register post
+  router.post("/register", (req, res) => {
+
+  })
+
   return router
 }
-
-
-
-
-
-
-
-//   router.get("/login_register", (req, res) => {
-//     // const id = req.session.userID;
-//     // console.log("id", id)
-//     db.query(`SELECT * FROM users WHERE id = $1`, [id])
-//       .then(data => {
-//         console.log("data", data)
-//     })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .send("Error: " + err.message);
-//       });
-//   });
-//   return router;
-// };
-
-// const login = function(email, password) {
-
-// }
-
-// router.post("/login_register", (req, res) => {
-//   const { email, password } = req.body;
-//   .then {
-
-//   }
-// const id = req.session.userID
-//     db.query(`SELECT * FROM users WHERE id = $1`, [id])
-//       .then(data => {
-//         const user = data.rows[0]
-//         console.log(user)
-//         res.render('profiles', { user });
-//       })
