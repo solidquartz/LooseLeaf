26eb4f6edbbd692998e88cec68615cae2bba0a24
megan <meganambermcbride@gmail.com>
2022-07-05 15:03:41 -0400

    Set up login get, login post and register post skeleton

--- routes/login_register.js
+++ routes/login_register.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const { user } = require('pg/lib/defaults');
 const router = express.Router();
+const helperFunctions = require('./helper_functions')
 
 module.exports = (db) => {
-  router.get("/login_register", (req, res))
-}
+  const getUserByEmail = (email) => {
+    return db.query(`SELECT * FROM users`)
+    .then(res => {
+      let currentUser = null;
+      for(const user of res.rows) {
+        if(user.email.toLowerCase() === email.toLowerCase)
+        currentUser = user
+        break;
+      }
+      return user
+    })
+    .catch(err => {
+      return err.message
+    })
+  }
 
-
-
-
-
-// Helper function to find user by email
-const getUserByEmail = (email) => {
-  return db.query(`SELECT * FROM users`)
-  .then(res => {
-    let currentUser = null;
-    for(const user of res.rows) {
-      if(user.email.toLowerCase() === email.toLowerCase)
-      currentUser = user
-      break;
-    }
-    return user
+  router.get("/", (req, res) => {
+    const id = req.session.userID;
+    res.render("login_register")
   })
-  .catch(err => {
-    return err.message
+
+
+  router.post("/login", (req, res) => {
+    const { email, password } = req.body
+    console.log(req.body, "here hey are")
   })
+  router.post("/register", (req, res) => {})
+  return router
 }
-
-
-
-
 
 
 
@@ -43,7 +46,6 @@ const getUserByEmail = (email) => {
 //     db.query(`SELECT * FROM users WHERE id = $1`, [id])
 //       .then(data => {
 //         console.log("data", data)
-//       res.render('/login_register');
 //     })
 //       .catch(err => {
 //         res
