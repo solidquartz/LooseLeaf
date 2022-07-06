783940f9114b3e6f162c02b1dac87f48024eb0cf
megan <meganambermcbride@gmail.com>
2022-07-06 09:56:29 -0400

    Add update route

--- routes/profiles.js
+++ routes/profiles.js
@@ -9,14 +9,32 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/:id", (req, res) => {
-    const id = req.session.userID;
 
-    const templateVars = {
-      id,
-    }
+  const updateUserInfo = (name, email, password, id) => {
+    return db.query(`UPDATE users
+    SET name = $1, email = $2, password = $3
+    WHERE id = $4
+    RETURNING *;`, [name, email, password, id])
+      .catch((err) => err.message);
+  };
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id
+    const templateVars = { id }
     res.render('profiles', templateVars)
+    return;
   });
+
+  router.post("/", (req, res) => {
+    const { name, email, password } = req.body
+    const id = req.session.userId
+
+    updateUserInfo(name, email, password, id)
+      .then
+      const templateVars = { id }
+      res.redirect(`/resources/my_resources/${id}`, templateVars);
+      return;
+  })
 
   return router;
 };
